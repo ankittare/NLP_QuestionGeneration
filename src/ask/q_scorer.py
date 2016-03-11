6252c666d315db71cdcc88fbc41716a3013919e2
Ankit Tare <ankit.vader@gmail.com>
2016-03-11 03:25:47 -0500

    Update q_scorer.py

--- src/ask/q_scorer.py
+++ src/ask/q_scorer.py
@@ -8,19 +8,14 @@
 #
 
 import kenlm
-
-### CONSTANTS ###
-
 # Location of the training data corpus
 CORPUS_FILENAME = '../corpus/BrownCorpus.binary'
-
 # Target number of words in an ideal question
 TARGET_LENGTH = 10
 # Weight to put on considering target length, in the open interval (0,1)
 LENGTH_WEIGHT = 0.25
 
 class QuestionScorer(object):
-    
     # The constructor initializes the language model which is used
     # to analyze the fluency of the questions.
     def __init__(self):
@@ -32,17 +27,14 @@ class QuestionScorer(object):
     # the language model.  Higher (less negative) numbers correspond to better
     # fluency.
     def score(self, questionToks):
-        
         # Join the tokens together with whitespace.  It is necessary that each
         # token be separated by whitespace, which is why the input is a list
         # and not a string.
         q_raw = ' '.join(questionToks)
-        
         baselineScore = self.model.score(q_raw)
         # We can possibly add more factors than just the baseline
         # probabalistic model here, but for right now this score should serve
         # us well.
-        
         # Add a parabolic weighting function which penalizes questions that
         # are too short or too long.
         s = baselineScore * (1 + LENGTH_WEIGHT*(len(questionToks) - TARGET_LENGTH)**2)
